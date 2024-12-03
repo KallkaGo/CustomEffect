ff67d226519329bb8c9b03fa95bb671e4cf9a297
More <765830637@qq.com>
2024-12-04 00:01:08 +0800

    chore: delete test function

--- src/three/components/Sketch/base/shader/points/fragment.glsl
+++ src/three/components/Sketch/base/shader/points/fragment.glsl
@@ -3,18 +3,8 @@
 varying vec4 pointsData;
 varying float vNowTime;
 varying float alpha;
-varying float vtest;
 
 uniform sampler2D diffuse;
-
-vec2 fun1(vec2 param1, float param2) {
-  float param5 = sin(param2);
-  float param4 = cos(param2);
-  float param7 = param5 * -1.;
-  mat2 param8 = mat2(param4, param7, param5, param4);
-  vec2 param10 = param8 * param1;
-  return param10;
-}
 
 void main() {
   vec2 uv = pointsData.xy;
