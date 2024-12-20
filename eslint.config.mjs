00b67454fb106e5560fe7940087e06a61dc23034
More <765830637@qq.com>
2024-12-20 11:02:49 +0800

    chore: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   react: true,
-  formatters: {
-    /**
-     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-     * By default uses Prettier
-     */
-    css: true,
-    /**
-     * Format HTML files
-     * By default uses Prettier
-     */
-    html: true,
-    /**
-     * Format Markdown files
-     * Supports Prettier and dprint
-     * By default uses Prettier
-     */
-    markdown: 'prettier',
-  },
+  formatters: true,
   rules: {
     'react-refresh/only-export-components': 'off',
     'unused-imports/no-unused-imports': 'warn',
