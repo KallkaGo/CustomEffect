cb779f590f213cf5352a6dec4384444cbac5f1dc
More <765830637@qq.com>
2025-01-12 00:03:00 +0800

    feat: boolean operations

--- src/three/components/Effect/shader/Sdf/fragment.glsl
+++ src/three/components/Effect/shader/Sdf/fragment.glsl
@@ -58,7 +58,34 @@ float sdHexagram(in vec2 p, in float r) {
   return length(p) * sign(p.y);
 }
 
+float opUnion(float d1, float d2) {
+  return min(d1, d2);
+}
+
+float opSubtraction(float d1, float d2) {
+  return max(-d1, d2);
+}
+
+float opIntersection(float d1, float d2) {
+  return max(d1, d2);
+}
+
+float softMax(float a, float b, float k) {
+  return log(exp(k * a) + exp(k * b)) / k;
+}
+
+float softMin(float a, float b, float k) {
+  return -softMax(-a, -b, k);
+}
+
+float softMinValue(float a, float b, float k) {
+  float h = exp(-b * k) / (exp(-a * k) + exp(-b * k));
+  // float h = remap(a - b, -1.0 / k, 1.0 / k, 0.0, 1.0);
+  return h;
+}
+
 const vec3 RED = vec3(1.0, 0.1, 0.1);
+const vec3 BLUE = vec3(0.1, 0.1, 1.0);
 
 mat2 rotate2D(float angle) {
   float s = sin(angle);
@@ -74,29 +101,41 @@ void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
   color = drawGrid(color, vec3(0.4), 10., 1., uv);
   color = drawGrid(color, vec3(0.0), 100., 2., uv);
 
-  float d = sdfCircle(pixelCoord, 100.);
+  // float d = sdfCircle(pixelCoord, 100.);
 
   /* base to games101 first translate second rotate   */
-  vec2 bPos = pixelCoord - vec2(300., 300.);
+  // vec2 bPos = pixelCoord - vec2(300., 300.);
 
-  bPos *= rotate2D(time * .25);
+  // bPos *= rotate2D(time * .25);
 
-  float t = sdfLine(pixelCoord, vec2(-200., -100.), vec2(300., -200.));
+  // float t = sdfLine(pixelCoord, vec2(-200., -100.), vec2(300., -200.));
 
-  float b = sdfBox(bPos, vec2(300., 100.));
+  // float b = sdfBox(bPos, vec2(300., 100.));
 
-  float s = sdHexagram(pixelCoord - vec2(-300., 300.), 100.);
+  // float s = sdHexagram(pixelCoord - vec2(-300., 300.), 100.);
 
-  color = mix(RED * 0.2, color, smoothstep(-1., 1., d));
-  color = mix(RED, color, smoothstep(-5., 0., d));
+  // color = mix(RED * 0.2, color, smoothstep(-1., 1., d));
+  // color = mix(RED, color, smoothstep(-5., 0., d));
 
-  color = mix(RED, color, smoothstep(4., 6., t));
+  // color = mix(RED, color, smoothstep(4., 6., t));
 
-  color = mix(RED * 0.2, color, smoothstep(-1., 1., b));
-  color = mix(RED, color, smoothstep(-5., 0., b));
+  // color = mix(RED * 0.2, color, smoothstep(-1., 1., b));
+  // color = mix(RED, color, smoothstep(-5., 0., b));
 
-  color = mix(RED * 0.2, color, smoothstep(-1., 1., s));
-  color = mix(RED, color, smoothstep(-5., 0., s));
+  // color = mix(RED * 0.2, color, smoothstep(-1., 1., s));
+  // color = mix(RED, color, smoothstep(-5., 0., s));
+
+  float box = sdfBox(rotate2D(time * 0.5) * pixelCoord, vec2(200.0, 100.0));
+  float d1 = sdHexagram(pixelCoord - vec2(-300.0, -150.0), 70.0);
+  float d2 = sdHexagram(pixelCoord - vec2(300.0, -150.0), 70.0);
+  float d3 = sdHexagram(pixelCoord - vec2(0.0, 300.0), 70.0);
+  float d = opUnion(opUnion(d1, d2), d3);
+
+  vec3 sdfColour = mix(RED, BLUE, smoothstep(0.0, 1.0, softMinValue(box, d, 0.01)));
+
+  d = softMin(box, d, .05);
+  color = mix(sdfColour * 0.2, color, smoothstep(-1.0, 1.0, d));
+  color = mix(sdfColour, color, smoothstep(-5.0, 0.0, d));
 
   outputColor = vec4(color, 1.0);
 
