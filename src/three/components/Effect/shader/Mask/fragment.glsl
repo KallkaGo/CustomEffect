68e7ef57d932d7dd3b747f6b46f19845d6e7ae59
KallkaGo <765830637@qq.com>
2025-03-29 16:04:40 +0800

    refactor: mask grayColor

--- src/three/components/Effect/shader/Mask/fragment.glsl
+++ src/three/components/Effect/shader/Mask/fragment.glsl
@@ -21,30 +21,20 @@ float sdfCircle(vec2 p, float r) {
 // https://iquilezles.org/
 //
 // https://www.shadertoy.com/view/Xsl3Dl
-vec3 hash( vec3 p ) // replace this by something better
+vec3 hash(vec3 p) // replace this by something better
 {
-	p = vec3( dot(p,vec3(127.1,311.7, 74.7)),
-            dot(p,vec3(269.5,183.3,246.1)),
-            dot(p,vec3(113.5,271.9,124.6)));
+  p = vec3(dot(p, vec3(127.1, 311.7, 74.7)), dot(p, vec3(269.5, 183.3, 246.1)), dot(p, vec3(113.5, 271.9, 124.6)));
 
-	return -1.0 + 2.0*fract(sin(p)*43758.5453123);
+  return -1.0 + 2.0 * fract(sin(p) * 43758.5453123);
 }
 
-float noise( in vec3 p )
-{
-  vec3 i = floor( p );
-  vec3 f = fract( p );
-	
-	vec3 u = f*f*(3.0-2.0*f);
+float noise(in vec3 p) {
+  vec3 i = floor(p);
+  vec3 f = fract(p);
 
-  return mix( mix( mix( dot( hash( i + vec3(0.0,0.0,0.0) ), f - vec3(0.0,0.0,0.0) ), 
-                        dot( hash( i + vec3(1.0,0.0,0.0) ), f - vec3(1.0,0.0,0.0) ), u.x),
-                   mix( dot( hash( i + vec3(0.0,1.0,0.0) ), f - vec3(0.0,1.0,0.0) ), 
-                        dot( hash( i + vec3(1.0,1.0,0.0) ), f - vec3(1.0,1.0,0.0) ), u.x), u.y),
-              mix( mix( dot( hash( i + vec3(0.0,0.0,1.0) ), f - vec3(0.0,0.0,1.0) ), 
-                        dot( hash( i + vec3(1.0,0.0,1.0) ), f - vec3(1.0,0.0,1.0) ), u.x),
-                   mix( dot( hash( i + vec3(0.0,1.0,1.0) ), f - vec3(0.0,1.0,1.0) ), 
-                        dot( hash( i + vec3(1.0,1.0,1.0) ), f - vec3(1.0,1.0,1.0) ), u.x), u.y), u.z );
+  vec3 u = f * f * (3.0 - 2.0 * f);
+
+  return mix(mix(mix(dot(hash(i + vec3(0.0, 0.0, 0.0)), f - vec3(0.0, 0.0, 0.0)), dot(hash(i + vec3(1.0, 0.0, 0.0)), f - vec3(1.0, 0.0, 0.0)), u.x), mix(dot(hash(i + vec3(0.0, 1.0, 0.0)), f - vec3(0.0, 1.0, 0.0)), dot(hash(i + vec3(1.0, 1.0, 0.0)), f - vec3(1.0, 1.0, 0.0)), u.x), u.y), mix(mix(dot(hash(i + vec3(0.0, 0.0, 1.0)), f - vec3(0.0, 0.0, 1.0)), dot(hash(i + vec3(1.0, 0.0, 1.0)), f - vec3(1.0, 0.0, 1.0)), u.x), mix(dot(hash(i + vec3(0.0, 1.0, 1.0)), f - vec3(0.0, 1.0, 1.0)), dot(hash(i + vec3(1.0, 1.0, 1.0)), f - vec3(1.0, 1.0, 1.0)), u.x), u.y), u.z);
 }
 
 float fbm(vec3 p, int octaves, float persistence, float lacunarity) {
@@ -53,7 +43,7 @@ float fbm(vec3 p, int octaves, float persistence, float lacunarity) {
   float total = 0.0;
   float normalization = 0.0;
 
-  for (int i = 0; i < octaves; ++i) {
+  for(int i = 0; i < octaves; ++i) {
     float noiseValue = noise(p * frequency);
     total += noiseValue * amplitude;
     normalization += amplitude;
@@ -67,6 +57,12 @@ float fbm(vec3 p, int octaves, float persistence, float lacunarity) {
   return total;
 }
 
+vec4 graySampler(vec4 sampler) {
+
+  vec3 color = sampler.rgb;
+
+  return vec4(vec3(luminance(color.rgb)), sampler.a);
+}
 
 void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
 
@@ -76,21 +72,21 @@ void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
 
   vec2 pixelCoords = uv * resolution;
 
-  float noiseSample = fbm(vec3(pixelCoords, 0.0) * 0.005 +time *.1 , 4, 1., 2.0);
+  float noiseSample = fbm(vec3(pixelCoords, 0.0) * 0.005 + time * .1, 4, 1., 2.0);
 
   vec4 sampler1 = texture2D(uChannel0, newUV);
 
-  vec4 sampler2 = vec4(vec3(luminance(sampler1.rgb)), sampler1.a);
+  vec4 sampler2 = graySampler(sampler1);
 
-  float d = sdfCircle(pixelCoords - mousePos , 50. + noiseSample * 150.);
+  float d = sdfCircle(pixelCoords - mousePos, 50. + noiseSample * 150.);
 
   vec4 color = sampler2;
 
-  color = mix(sampler1,color,smoothstep(0.,2.,d));
+  color = mix(sampler1, color, smoothstep(0., 2., d));
 
   // glow
   float glowAmount = smoothstep(0., 32., abs(d));
-  glowAmount = 1. - pow(glowAmount,.125);
+  glowAmount = 1. - pow(glowAmount, .125);
 
   vec3 glowColor = vec3(0.);
 
@@ -107,6 +103,6 @@ void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
   #endif
 
   color.rgb += glowAmount * glowColor;
-  
+
   outputColor = color;
 }
